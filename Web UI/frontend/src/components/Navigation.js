a7199f07d4291d8ddd1e6a7f99bc1a7df23eea34
Eric Alpizar <ericalpizar@icloud.com>
2021-06-20 22:07:28 -0600

    Improved navigation.js

--- Web UI/frontend/src/components/Navigation.js
+++ Web UI/frontend/src/components/Navigation.js
@@ -12,14 +12,14 @@ export default class Navigation extends Component {
             <Navbar bg="dark" variant="dark">
              <CloudLightningRain className="ml-1" />
              
-            <Navbar.Brand href="http://localhost:3000/Decada_continente">Climate Crawler</Navbar.Brand>
+            <Navbar.Brand href="/Decada_continente">Climate Crawler</Navbar.Brand>
             <Nav className="mr-auto">
 
-             <Nav.Link href="http://localhost:3000/Decada_continente">Continentes</Nav.Link>
-             <Nav.Link href="http://localhost:3000/cont_pais_var">Estadisticas por continente</Nav.Link>
-              <Nav.Link href="http://localhost:3000/max_prome">Promedios</Nav.Link>
-              <Nav.Link href="http://localhost:3000/estacion">Estacion</Nav.Link>
-              <Nav.Link href="http://localhost:3000/ano_min_max_var">Estadisticas por año</Nav.Link>
+             <Nav.Link href="/Decada_continente">Continentes</Nav.Link>
+             <Nav.Link href="/cont_pais_var">Estadisticas por continente</Nav.Link>
+              <Nav.Link href="/max_prome">Promedios</Nav.Link>
+              <Nav.Link href="/estacion">Estacion</Nav.Link>
+              <Nav.Link href="/ano_min_max_var">Estadisticas por año</Nav.Link>
               
               
               
